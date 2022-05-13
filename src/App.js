5352516c0e96d7afce803ea40f873ae67f71ced4
Nahuel61920 <estebancarrizo619@gmail.com>
2022-05-12 22:47:58 -0300

    Create Digital Clock in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [time, setTime] = useState("")
+
+  function formatTime(val) {
+    if (val < 10) {
+      return "0";
+    } else {
+      return "";
+    }
+  }
+
+  useState(() => {
+    const timeID = setInterval(
+      () => tick(), 1000
+    )
+
+    return function cleanup() {
+      clearImmediate(timeID);
+    }
+  })
+
+  function tick() {
+    const d = new Date();
+    const h = d.getHours();
+    const m = d.getMinutes();
+    const s = d.getSeconds();
+
+    setTime(formatTime(h) + h + ":" + formatTime(m) + m + ":" + formatTime(s) + s)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <div className="clock">
+        <div className="screen">
+          <h1 className="time">{time}</h1>
+        </div>
+      </div>
     </div>
   );
 }
